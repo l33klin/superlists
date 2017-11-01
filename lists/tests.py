56777af23a6addfa3f26fe82249ae65b5003d607
jian.li <jian.li@shopeemobile.com>
2017-11-01 16:42:33 +0800

    add test code try to save user input

--- lists/tests.py
+++ lists/tests.py
@@ -23,9 +23,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), execpted_html)
 
     def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
+        response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
