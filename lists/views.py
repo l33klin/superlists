b5f9a590294f413fd197ae758a8eb46464ad7bce
lijian16 <lijian16@baidu.com>
2017-11-07 01:28:05 +0800

    add new_list API test case

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,10 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     new_item_text = request.POST['item_text']
+    #     Item.objects.create(text=new_item_text)
+    #     return redirect('/lists/the-only-list-in-the-world/')
 
     return render(request, 'home.html')
 
@@ -16,4 +16,10 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    new_item_text = request.POST['item_text']
+    Item.objects.create(text=new_item_text)
+    return redirect('/lists/the-only-list-in-the-world/')
 
