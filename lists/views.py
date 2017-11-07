f3393e5dfdf78676a17b89877046ec3fa81d87f1
lijian16 <lijian16@baidu.com>
2017-11-07 22:53:35 +0800

    i am totally massed, don't know what i do

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -19,7 +19,8 @@ def view_list(request):
 
 
 def new_list(request):
+    list_ = List.objects.create()
     new_item_text = request.POST['item_text']
-    Item.objects.create(text=new_item_text)
+    Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
