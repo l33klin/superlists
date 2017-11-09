a629615d91916103c01cfecd0c15cd827ee2c702
lijian16 <lijian16@baidu.com>
2017-11-10 01:02:45 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 # from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -11,6 +12,21 @@ MAX_WAIT = 10
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            print(arg)
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[2]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         # self.browser = browser = webdriver.Firefox()
@@ -40,7 +56,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         self.assertIn('To-Do', self.browser.title)
 
@@ -93,7 +109,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visit home page
         # can not see Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -116,7 +132,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith visit the website
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         time.sleep(1)
 
