2ca95e626e0c2d919a320dbd6c1b50605e29a06a
jian.li <jian.li@shopeemobile.com>
2017-11-15 19:23:41 +0800

    add docker change temp

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'mysecretpassword',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
