c465ea3554029de43e7cf4f2ace5390b77a15501
l33klin <l33klin@gmail.com>
2017-11-27 19:17:46 +0800

    add proxy check, if there is more than one proxy, raise error

--- superlists/lists/requests_proxy.py
+++ superlists/lists/requests_proxy.py
@@ -9,10 +9,34 @@
 """
 
 import requests
+from functools import wraps
 
 
 proxies = {'http': 'http://127.0.0.1:7777',
            'https': 'http://127.0.0.1:7777'}
+
+
+class MultiProxyException(Exception):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @staticmethod  # known case of __new__
+    def __new__(*args, **kwargs):  # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+
+def proxy_check(func):
+
+    @wraps(func)
+    def function_check(*args, **kwargs):
+        if 'proxy' in kwargs:
+            raise MultiProxyException("More than one proxy argument is passed!")
+        result = func(*args, **kwargs)
+        return result
+
+    return function_check
 
 
 class ProxyRequests(object):
@@ -21,29 +45,36 @@ class ProxyRequests(object):
     """
 
     @staticmethod
+    @proxy_check
     def post(url, data=None, json=None, **kwargs):
         requests.post(url, data=data, json=json, proxy=proxies, **kwargs)
 
     @staticmethod
+    @proxy_check
     def get(url, params=None, **kwargs):
         requests.get(url, params=params, proxy=proxies, **kwargs)
 
     @staticmethod
+    @proxy_check
     def options(url, **kwargs):
         requests.options(url, proxy=proxies, **kwargs)
 
     @staticmethod
+    @proxy_check
     def head(url, **kwargs):
         requests.head(url, proxy=proxies, **kwargs)
 
     @staticmethod
+    @proxy_check
     def put(url, data=None, **kwargs):
         requests.put(url, data=data, proxy=proxies, **kwargs)
 
     @staticmethod
+    @proxy_check
     def patch(url, data=None, **kwargs):
         requests.patch(url, data=data, proxy=proxies, **kwargs)
 
     @staticmethod
+    @proxy_check
     def delete(url, **kwargs):
         requests.delete(url, proxy=proxies, **kwargs)
