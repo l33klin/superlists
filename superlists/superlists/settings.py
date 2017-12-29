727f8935fd27a329dfce2f640715b0e1cbd86e1a
l33klin <l33klin@gmail.com>
2017-12-29 18:19:17 +0800

    add SOCIAL_AUTH_PIPLINE

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -161,3 +161,16 @@ LOGIN_REDIRECT_URL = 'home'  # redirect the request which login without next par
 
 SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY')
 SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET')
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',  # <--- Associate users by email
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
